7e32ac12bf58ee6dd17ce3195e99f5626e030161
JusticeIL <galru3@mta.ac.il>
2026-04-15 19:16:59 +0300

    added a closing bracket at the end of the class

--- Ex01 Gal 209385509 Lihi 314958042/Ex01_2/Program.cs
+++ Ex01 Gal 209385509 Lihi 314958042/Ex01_2/Program.cs
@@ -14,7 +14,10 @@ namespace Ex01_2
         {
             if (treeDepth < 4 || treeDepth > 15)
             {
-                string invalidMessage = string.Format("The tree depth entered ({0}) is invalid. Please enter a new tree depth between 4 and 15.", treeDepth);
+                string invalidMessage =
+                    string.Format(
+                        "The tree depth entered ({0}) is invalid. Please enter a new tree depth between 4 and 15.",
+                        treeDepth);
                 Console.WriteLine(invalidMessage);
                 return;
             }
@@ -36,40 +39,43 @@ namespace Ex01_2
             printRoot(currentLevel, currentCharToPrint, maxWidth);
         }
 
-    private static void printLine(int currentLevel, ref char currentCharToPrint, int currentWidth, int maxWidth)
-    {
-        string baseTreeLine = string.Format("{0} ", currentLevel);
-        StringBuilder treeLine = new StringBuilder(baseTreeLine);
-        int currentLettersAmount = 1;
-        string spacesInTreeLine = maxWidth - currentWidth - 1 >= 0 ? new string(' ', (maxWidth - currentWidth - 1) : new string('\b', 1));
-
-        treeLine.Append(spacesInTreeLine);
-        treeLine.Append(' ');
-        while (currentLettersAmount <= currentWidth)
+        private static void printLine(int currentLevel, ref char currentCharToPrint, int currentWidth, int maxWidth)
         {
-            treeLine.Append(currentCharToPrint);
+            string baseTreeLine = string.Format("{0} ", currentLevel);
+            StringBuilder treeLine = new StringBuilder(baseTreeLine);
+            int currentLettersAmount = 1;
+            string spacesInTreeLine = maxWidth - currentWidth - 1 >= 0
+                ? new string(' ', (maxWidth - currentWidth - 1) : new string('\b', 1));
+
+            treeLine.Append(spacesInTreeLine);
             treeLine.Append(' ');
-            currentCharToPrint = getNextChar(currentCharToPrint);
-            currentLettersAmount++;
+            while (currentLettersAmount <= currentWidth)
+            {
+                treeLine.Append(currentCharToPrint);
+                treeLine.Append(' ');
+                currentCharToPrint = getNextChar(currentCharToPrint);
+                currentLettersAmount++;
+            }
+
+            treeLine.Append(spacesInTreeLine);
+            Console.WriteLine(treeLine.ToString());
         }
-        treeLine.Append(spacesInTreeLine);
-        Console.WriteLine(treeLine.ToString());
-    }
 
         private static char getNextChar(char currentChar)
         {
             return currentChar == 'Z' ? 'A' : (char)(currentChar + 1);
         }
 
-    private static void printRoot(int currentLevel, char currentCharToPrint, int maxWidth)
-    {
-        string baseRootLine = string.Format("{0} ", currentLevel);
-        StringBuilder rootLine = new StringBuilder(baseRootLine);
-        string spacesInRootLine = new string(' ', ((maxWidth / 2) - 1) * 2 - 1);
+        private static void printRoot(int currentLevel, char currentCharToPrint, int maxWidth)
+        {
+            string baseRootLine = string.Format("{0} ", currentLevel);
+            StringBuilder rootLine = new StringBuilder(baseRootLine);
+            string spacesInRootLine = new string(' ', ((maxWidth / 2) - 1) * 2 - 1);
 
-        rootLine.Append(spacesInRootLine);
-        rootLine.Append(string.Format("|{0}|", currentCharToPrint));
-        rootLine.Append(spacesInRootLine);
-        Console.WriteLine(rootLine.ToString());
+            rootLine.Append(spacesInRootLine);
+            rootLine.Append(string.Format("|{0}|", currentCharToPrint));
+            rootLine.Append(spacesInRootLine);
+            Console.WriteLine(rootLine.ToString());
+        }
     }
 }
