8a24d5735933144fb9542976068b705223f7911c
lihicohen122 <lihicohen122@gmail.com>
2026-04-15 11:24:44 +0300

    Initialized Ex01 and added gitignore

---
+++ Ex01 Gal 209385509 Lihi 314958042/Ex01_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_2
+{
+    internal class Program
+    {
+    }
+}
