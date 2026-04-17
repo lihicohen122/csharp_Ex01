310330156787abb0425cfe69f4543d009ad004b4
lihicohen122 <lihicohen122@gmail.com>
2026-04-17 11:29:29 +0300

    Made templates

---
+++ Ex01 Gal 209385509 Lihi 314958042/Ex01_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_4
+{
+    internal class Program
+    {
+    }
+}
