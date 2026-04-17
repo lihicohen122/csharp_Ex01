f80ad3ea55ca4a06df0623ca2ce4e25ba670ebf5
lihicohen122 <lihicohen122@gmail.com>
2026-04-17 14:02:45 +0300

    Added All english letters checker (instead of using All from System.Linq) and polished.

--- Ex01 Gal 209385509 Lihi 314958042/Ex01_4/Program.cs
+++ Ex01 Gal 209385509 Lihi 314958042/Ex01_4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex01_4
 {
@@ -19,14 +15,14 @@ namespace Ex01_4
 
             printIsStringPalindrome(userInputString.ToLower());
             printIsNumberDivisibleByFourIfStringIsNumber(userInputString);
-
-            bool areAllCharactersEnglishLetters = userInputString.All(char.IsLetter);
+            bool areAllCharactersEnglishLetters = isAllEnglishLetters(userInputString);
 
             if(areAllCharactersEnglishLetters)
             {
                 printNumberOfUpperCaseLettersIfStringIsAllEnglishLetters(userInputString);
                 printIsReversedAlphabeticalOrderIfStringIsAllEnglishLetters(userInputString);
             }
+
         }
 
         private static string printRequirementMessageCheckValidityAndGetUserInput()
@@ -34,11 +30,12 @@ namespace Ex01_4
             Console.WriteLine("Please enter an 8 character string: ");
             string userInputString = Console.ReadLine();
 
-            while (userInputString == null || userInputString.Length != 8)
+            while(userInputString == null || userInputString.Length != 8)
             {
                 Console.WriteLine("Invalid input! Please enter an 8 character string: ");
                 userInputString = Console.ReadLine();
             }
+
             return userInputString;
         }
 
@@ -56,7 +53,8 @@ namespace Ex01_4
             }
             else
             {
-                isPalindromeResult = isUserInputPalindrome(i_UserStringInput.Substring(1, i_UserStringInput.Length - 2));
+                isPalindromeResult =
+                    isUserInputPalindrome(i_UserStringInput.Substring(1, i_UserStringInput.Length - 2));
             }
 
             return isPalindromeResult;
@@ -67,6 +65,7 @@ namespace Ex01_4
             bool isPalindrome = isUserInputPalindrome(i_UserInputString);
             string isPalindromeSubString = isPalindrome ? "" : "not ";
             string palindromeMessage = string.Format("The string is {0}a palindrome.", isPalindromeSubString);
+
             Console.WriteLine(palindromeMessage);
         }
 
@@ -74,10 +73,13 @@ namespace Ex01_4
         {
             // bool areAllCharactersDigits = userInputString.All(char.IsDigit);
             bool areAllCharactersDigits = int.TryParse(i_UserInputString, out int userInputNumber);
-            if (areAllCharactersDigits)
+
+            if(areAllCharactersDigits)
             {
                 string isFourDivisibleSubString = userInputNumber % 4 == 0 ? "" : "not ";
-                string fourDivisibleMessage = string.Format("The number is {0}divisible by 4.", isFourDivisibleSubString);
+                string fourDivisibleMessage = string.Format(
+                    "The number is {0}divisible by 4.",
+                    isFourDivisibleSubString);
                 Console.WriteLine(fourDivisibleMessage);
             }
         }
@@ -86,16 +88,36 @@ namespace Ex01_4
         {
             int numOfUpperCaseLetters = 0;
 
-            for (int i = 0; i < i_UserInputString.Length; i++)
+            for(int i = 0; i < i_UserInputString.Length; i++)
             {
-                if (char.IsUpper(i_UserInputString[i]))
+                if(char.IsUpper(i_UserInputString[i]))
                 {
                     numOfUpperCaseLetters++;
                 }
             }
 
-            string upperCaseLettersMessage = string.Format("The number of uppercase letters in the string is: {0}.", numOfUpperCaseLetters);
+            string upperCaseLettersMessage = string.Format(
+                "The number of uppercase letters in the string is: {0}.",
+                numOfUpperCaseLetters);
+
             Console.WriteLine(upperCaseLettersMessage);
+        }
+
+        private static bool isAllEnglishLetters(string i_UserInputString)
+        {
+            bool isAllEnglishLetters = true;
+
+            for (int i = 0; i < i_UserInputString.Length; i++)
+            {
+                if ((i_UserInputString[i] < 'A' || i_UserInputString[i] > 'Z')
+                    && (i_UserInputString[i] < 'a' || i_UserInputString[i] > 'z'))
+                {
+                    isAllEnglishLetters = false;
+                    break;
+                }
+            }
+
+            return isAllEnglishLetters;
         }
 
         private static void printIsReversedAlphabeticalOrderIfStringIsAllEnglishLetters(string i_UserInputString)
@@ -103,17 +125,23 @@ namespace Ex01_4
             string allLowerCasedString = i_UserInputString.ToLower();
             bool isStringInReverseAlphabeticalOrder = true;
 
-            for (int i = 0; i < allLowerCasedString.Length - 1; i++)
+            for(int i = 0; i < allLowerCasedString.Length - 1; i++)
             {
-                if (allLowerCasedString[i] < allLowerCasedString[i + 1])
+                if(allLowerCasedString[i] < allLowerCasedString[i + 1])
                 {
                     isStringInReverseAlphabeticalOrder = false;
                     break;
                 }
             }
+
             string reverseAlphabeticalOrderSubString = isStringInReverseAlphabeticalOrder ? "" : "not ";
-            string reverseAlphabeticalOrderMessage = string.Format("The string is {0}in reverse alphabetical order.", reverseAlphabeticalOrderSubString);
+            string reverseAlphabeticalOrderMessage = string.Format(
+                "The string is {0}in reverse alphabetical order.",
+                reverseAlphabeticalOrderSubString);
+
             Console.WriteLine(reverseAlphabeticalOrderMessage);
         }
-    }
+    } 
+
+
 }
